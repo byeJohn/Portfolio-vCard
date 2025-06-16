c16c2811728e95aa0ef82d18cd7a044957c20b4f
byeJohn <johnnguyen4001@gmail.com>
2025-06-16 03:24:56 -0400

    Update: portfolio content and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,12 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// Removed testimonials code
-
 
 
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-select-value]");
+const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
